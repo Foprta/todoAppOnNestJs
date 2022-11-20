45cc68b23b6f3cad8cf988b36efa6cc905b400a9
Evgenii Klinnikov <foprta@Foprtas-MacBook-Pro.local>
2022-11-20 15:32:53 +0400

    docs added

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -10,7 +10,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoPayload } from './interface/todo-payload.interface';
 import { TodoService } from './service/todo.service';
@@ -21,22 +21,28 @@ import { UpdateTodoDto } from './dto/update-todo.dto';
 export class TodoController {
   constructor(private todoService: TodoService) {}
 
+  @ApiOperation({ summary: "Get all ToDo's" })
   @Get()
   getAllTodo(): Promise<TodoPayload[]> {
     return this.todoService.getAllTodo();
   }
 
+  @ApiOperation({ summary: 'Create ToDo entity' })
   @Post()
   @UsePipes(ValidationPipe)
   createTodo(@Body() todoDto: CreateTodoDto): Promise<TodoPayload> {
     return this.todoService.createTodo(todoDto);
   }
 
+  @ApiOperation({ summary: 'Update ToDo entity' })
+  @ApiParam({ name: 'id', required: true, example: 12, description: 'ToDo ID you want to update' })
   @Patch('/:id')
   updateTodoById(@Param('id', ParseIntPipe) id: number, @Body() todoDto: UpdateTodoDto): Promise<TodoPayload> {
     return this.todoService.updateTodoById(id, todoDto);
   }
 
+  @ApiOperation({ summary: 'Delete ToDo entity' })
+  @ApiParam({ name: 'id', required: true, example: 12, description: 'ToDo ID you want to delete' })
   @Delete('/:id')
   deleteTodoById(@Param('id', ParseIntPipe) id: number): Promise<{ message: string }> {
     return this.todoService.deleteTodoById(id);
